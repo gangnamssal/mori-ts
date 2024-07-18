ffe3d4bcb03d72b56de73628b1dbab25b71de5f9
ggnsall <ggnsall@naver.com>
2024-07-18 11:13:07 +0900

    chor : npm 패키지 배포 설정 변경

--- src/core/curry/curry.spec.ts
+++ src/core/curry/curry.spec.ts
@@ -2,9 +2,17 @@ import curry from './curry';
 
 describe('curry', () => {
   it('should curry a function', () => {
-    // const add = curry((a: number, b: number) => a + b);
-    // const addOne = add(1);
-    // const addTwo = addOne(2);
-    // expect(addTwo).toBe(3);
+    const add = curry((a: number, b: number) => a + b);
+    const addOne = add(1);
+    const addTwo = addOne(2);
+    expect(addTwo).toBe(3);
+  });
+
+  it('should curry a function with multiple arguments', () => {
+    const add = curry((a: number, b: number, c: number) => a + b + c);
+    const addOne = add(1);
+    const addTwo = addOne(2);
+    const addThree = addTwo(3);
+    expect(addThree).toBe(6);
   });
 });
