f1aa9441ea0dc3b2311d45a7ac1c507623d5d3b0
ggnsall <ggnsall@naver.com>
2024-07-27 21:23:08 +0900

    add : concurrent 함수 생성

--- src/core/concurrent/concurrent.spec.ts
+++ src/core/concurrent/concurrent.spec.ts
@@ -1,18 +1,55 @@
-import { delay, delayEach, each, map, pipe, toArray, toAsync } from '..';
+import { delay, filter, map, pipe, range, toArray, toAsync } from '..';
 import concurrent from './concurrent';
 
 describe('concurrent', () => {
-  it('test', async () => {
+  it('limit is valid num with map', async () => {
+    const start = Date.now();
+
     const res = await pipe(
-      [1, 2, 3, 4],
+      range(0, 6),
       toAsync,
-      map(a => a),
-      delayEach(1000),
-      concurrent,
-      each(console.log),
+      map(x => delay(1000, x)),
+      concurrent(4),
       toArray,
     );
 
-    expect(res).toEqual([1, 2, 3, 4]);
+    const end = Date.now();
+
+    expect(res).toEqual([0, 1, 2, 3, 4, 5]);
+    expect(end - start).toBeLessThan(3000);
+  });
+
+  it('limit is valid num with map 2', async () => {
+    const start = Date.now();
+
+    const res = await pipe(
+      range(0, 6),
+      toAsync,
+      map(x => delay(1000, x * 2)),
+      concurrent(3),
+      toArray,
+    );
+
+    const end = Date.now();
+
+    expect(res).toEqual([0, 2, 4, 6, 8, 10]);
+    expect(end - start).toBeLessThan(4000);
+  });
+
+  it('limit is valid num with filter', async () => {
+    const start = Date.now();
+
+    const res = await pipe(
+      range(0, 6),
+      toAsync,
+      filter(x => delay(1000, x % 2 === 0)),
+      concurrent(4),
+      toArray,
+    );
+
+    const end = Date.now();
+
+    expect(res).toEqual([0, 2, 4]);
+    expect(end - start).toBeLessThan(3000);
   }, 10000);
 });
--- src/core/concurrent/concurrent.ts
+++ src/core/concurrent/concurrent.ts
@@ -1,33 +1,110 @@
-async function* concurrent<T>(...iters: AsyncIterable<T>[]): AsyncIterableIterator<T> {
-  const promises = iters.map(iter =>
-    (async function* () {
-      for await (const item of iter) {
-        yield item;
+import { IterableInfer, RejectType, ResolveType } from './../../types';
+
+function concurrent<A extends AsyncIterable<unknown>>(
+  limit: number,
+): (iter: A) => AsyncIterableIterator<IterableInfer<A>>;
+
+function concurrent<A extends AsyncIterable<unknown>>(
+  limit: number,
+  iter: A,
+): AsyncIterableIterator<IterableInfer<A>>;
+
+function concurrent<A extends AsyncIterable<unknown>>(
+  limit: number,
+  iter?: A,
+): AsyncIterableIterator<IterableInfer<A>> | ((iter: A) => AsyncIterableIterator<IterableInfer<A>>) {
+  if (!iter)
+    return (iter: A): AsyncIterableIterator<IterableInfer<A>> =>
+      concurrent(limit, iter) as AsyncIterableIterator<IterableInfer<A>>;
+
+  if (limit === Infinity || limit <= 0 || limit === -Infinity)
+    throw new Error('limit must be a positive number');
+
+  // 이터레이터
+  const iterator = iter[Symbol.asyncIterator]();
+
+  // 버퍼큐
+  const bufferQueue: PromiseSettledResult<IteratorResult<IterableInfer<A>, any>>[] = [];
+
+  // 작업이 완료된 큐
+  const settledQueue: [ResolveType<IterableInfer<A>>, RejectType][] = [];
+
+  // 이전 작업
+  let prevTask = Promise.resolve();
+
+  // 작업이 완료된 큐의 인덱스
+  let callCount = 0;
+
+  // 작업이 완료된 큐의 길이
+  let resolvedCount = 0;
+
+  // 작업이 완료되었는지 여부
+  let finished = false;
+
+  // 작업이 진행중인지 여부
+  let pending = false;
+
+  // 버퍼큐에 있는 작업을 실행
+  const taskQueue = () => {
+    while (bufferQueue.length > 0 && callCount > resolvedCount) {
+      const [resolve, reject] = settledQueue.shift() as [ResolveType<IterableInfer<A>>, RejectType];
+      const { status, value, reason } = bufferQueue.shift() as any;
+
+      if (status === 'fulfilled') {
+        resolve(value);
+        resolvedCount++;
+      } else {
+        reject(reason);
+        finished = true;
       }
-    })(),
-  );
-
-  for await (const item of mergeAsyncIterables(...promises)) {
-    yield item;
-  }
-}
-
-async function* mergeAsyncIterables<T>(...iters: AsyncIterable<T>[]): AsyncIterableIterator<T> {
-  const readers = iters.map(iter => iter[Symbol.asyncIterator]());
-  const results = readers.map(reader => reader.next());
-
-  while (readers.length > 0) {
-    const next = await Promise.race(results.map((promise, i) => promise.then(value => ({ value, i }))));
-    const { value, i } = next;
-
-    if (value.done) {
-      readers.splice(i, 1);
-      results.splice(i, 1);
-    } else {
-      results[i] = readers[i].next();
-      yield value.value;
     }
-  }
+  };
+
+  // 버퍼큐에 작업을 추가
+  const addBuffer = () => {
+    if (pending) {
+      prevTask = prevTask.then(() => void (!finished && callCount > resolvedCount && addBuffer()));
+    } else {
+      const nextItems = Promise.allSettled(Array.from({ length: limit }, () => iterator.next())) as Promise<
+        PromiseSettledResult<IteratorResult<IterableInfer<A>, any>>[]
+      >;
+
+      pending = true;
+      prevTask = prevTask
+        .then(() => nextItems)
+        .then(nextItems => {
+          bufferQueue.push(...nextItems);
+          pending = false;
+          recur();
+        });
+    }
+  };
+
+  // 재귀적으로 작업을 실행
+  const recur = async () => {
+    if (finished || callCount === resolvedCount) return;
+
+    if (bufferQueue.length > 0) return taskQueue();
+
+    return addBuffer();
+  };
+
+  return {
+    [Symbol.asyncIterator]() {
+      return this;
+    },
+
+    next() {
+      callCount++;
+
+      if (finished) return { done: true, value: undefined };
+
+      return new Promise((resolve, reject) => {
+        settledQueue.push([resolve, reject]);
+        recur();
+      });
+    },
+  } as AsyncIterableIterator<IterableInfer<A>>;
 }
 
 export default concurrent;
