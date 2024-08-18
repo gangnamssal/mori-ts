af6900460f2de730185a2d4a7ada51cbc0c3a316
김태헌 <gimtaeheon@gimtaeheon-ui-MacBookPro.local>
2024-08-18 16:43:48 +0900

    update : length 함수 리팩토링

--- src/core/length/length.ts
+++ src/core/length/length.ts
@@ -1,24 +1,10 @@
-import { ReturnIterableAsyncIterableType } from '../../types';
-import pipe from '../pipe/pipe';
 import reduce from '../reduce/reduce';
-import { isAsyncIterable, isIterable } from './../../utils';
-
-function syncLength<A>(iter: Iterable<A>): number {
-  return pipe(iter, iter => reduce(acc => acc + 1, 0, iter));
-}
-
-async function asyncLength<A>(iter: AsyncIterable<A>): Promise<number> {
-  return pipe(iter, iter => reduce(acc => acc + 1, 0, iter));
-}
+import { ReturnIterableAsyncIterableType } from '../../types';
 
 function length<A extends Iterable<unknown> | AsyncIterable<unknown>>(
   iter: A,
 ): ReturnIterableAsyncIterableType<A, number> {
-  if (isIterable(iter)) return syncLength(iter) as ReturnIterableAsyncIterableType<A, number>;
-
-  if (isAsyncIterable(iter)) return asyncLength(iter) as ReturnIterableAsyncIterableType<A, number>;
-
-  throw new Error('argument is not iterable or async iterable');
+  return reduce(acc => acc + 1, 0, iter);
 }
 
 export default length;
